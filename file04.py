3c43c803d68f9eba5dd735411fd02d93d6f18a7b
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-12 12:00:56 +0500

    done

--- file04.py
+++ file04.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    x = []
+    for i in data:
+        if not i.isdigit():
+            x.append(i)
+    return x
     
 # Read data from file
+print(main(open('txt_file/data04.txt','r+').read()))
